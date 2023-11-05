41ceace660ce152c9c82e80a9bf3ef015304095c
alukan <kolyaterenya@gmail.com>
2023-11-05 19:26:07 +0300

    auth-added

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,13 +1,20 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
- 
+import { notFound } from 'next/navigation';
+import { invoices } from '@/app/lib/placeholder-data';
+
 export default async function Page({ params }: { params: { id: string } }) {
-  const id = params.id;
-  const [invoice, customers] = await Promise.all([
-    fetchInvoiceById(id),
-    fetchCustomers(),
-  ]);
+    const id = params.id;
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id),
+        fetchCustomers(),
+    ]);
+
+    if (!invoice) {
+        notFound();
+      }
+
     return (
         <main>
             <Breadcrumbs
